13fae104bd1def10cdb51719f13d2f6cefb9e8cf
Lee HyunJae <dubu1993@snu.ac.kr>
2017-08-30 02:24:21 +0000

    minor fix

--- cuDNNTest/cuDNNTest.cu
+++ cuDNNTest/cuDNNTest.cu
@@ -52,24 +52,14 @@ int convertToIndex(int n, int batch_count, int k, int in_channel, int h, int in_
     return ((n * in_channel + k) * in_height + h) * in_width + w;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 	const int batch_count = 1;//입력 데이터 갯수, 배치사이즈
-	const int in_channel = 512;//입력 데이터의 채널 수
-	const int in_height = 38;//입력 데이터의 세로 길이
-	const int in_width = 38;//입력 데이터의 가로 길이
-	const int filter_width = 3;//컨볼루션 필터(가중치)의 가로 길이
-	const int filter_height = 3;//컨볼루션 필터(가중치)의 세로 길이
-	const int filter_num = 1024;//컨볼루션 필터(가중치) 갯수
 	const int padding_w = 1;//컨볼루션 패딩. 필터의 가로 세로 길이가 3이고 패딩이 1,1 이면 SAME Convolution이 된다
 	const int padding_h = 1;
 	const int stride_horizontal = 1;//컨볼루션 스트라이드
 	const int stride_vertical = 1;
-	const int src_len = batch_count*filter_num*in_height*in_width;
-//	float inData_NCHW[batch_count][in_channel][in_height][in_width];//host 입력 데이터 NCHW
-//	float outData[batch_count][filter_num][in_height][in_width];//host 출력 데이터
-//	float filterData[filter_num][in_channel][filter_height][filter_width];
-	float *hostArray = new float[src_len];
+	int in_channel, in_height, in_width,filter_width, filter_height,filter_num, src_len;
 	float *inData_d;//device 입력 데이터
 	float *outData_d, *outData1_d;//device 출력 데이터
 	float *filterData_d;//device 컨볼루션 필터 데이터
@@ -77,6 +67,20 @@ int main()
     long time_accum = 0;
     size_t free, total;
     //float *inData_NCHW, *outData, *filterData;
+    if(argc < 4)
+    {
+        fprintf(stderr, "usage : ./test [Input Height/Width] [Channel] [Filter Height/Width] [Filter Num]\n");
+        return 0;
+    }
+   
+    in_height = atoi(argv[1]);
+    in_width = atoi(argv[1]);
+    in_channel = atoi(argv[2]);
+    filter_width = atoi(argv[3]);
+    filter_height = atoi(argv[3]);
+    filter_num = atoi(argv[4]);
+	src_len = batch_count*filter_num*in_height*in_width;
+    
     int inSize = batch_count * in_channel * in_height * in_width;
     int outSize = batch_count * filter_num * in_height * in_width;
     int filterSize = filter_num * in_channel * filter_height * filter_width;
@@ -84,6 +88,10 @@ int main()
     float *inData_NCHW = (float *)malloc(sizeof(float) * inSize);
     float *outData = (float *)malloc(sizeof(float) * outSize);
     float *filterData = (float *)malloc(sizeof(float) * filterSize);
+	float *hostArray = new float[src_len];
+
+    cudaMemGetInfo(&free,&total); 
+    printf("%ld KB free of total %ld KB\n",free/1024,total/1024);
 
     //입력 데이터 셋팅
     for(int n = 0; n < batch_count; n++) {
@@ -122,7 +130,6 @@ int main()
 		}
 	}
 
-    printf("check!\n");
     //GPU 메모리 할당
 	checkCUDA(cudaMalloc((void**)&inData_d, inSize * sizeof(float)));
 	checkCUDA(cudaMalloc((void**)&outData_d, outSize * sizeof(float)));
@@ -154,7 +161,7 @@ int main()
 	checkCUDNN(cudnnSetTensor4dDescriptor(inTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, batch_count, in_channel, in_height, in_width));
 	checkCUDNN(cudnnSetFilter4dDescriptor(filterDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, filter_num, in_channel, filter_height, filter_width));
 	//컨볼루션의 패딩, 스트라이드, 컨볼루션 모드 등을 셋팅
-	checkCUDNN(cudnnSetConvolution2dDescriptor(convDesc, padding_h, padding_w, stride_vertical, stride_horizontal, 1, 1, CUDNN_CROSS_CORRELATION));
+	checkCUDNN(cudnnSetConvolution2dDescriptor(convDesc, padding_h, padding_w, stride_vertical, stride_horizontal, 1, 1, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT));
     
    
 	int out_n, out_c, out_h, out_w;
@@ -187,7 +194,7 @@ int main()
 				algo,
 				&sizeInBytes));
 
-	//cout << "sizeInBytes " << sizeInBytes << endl;
+	cout << "sizeInBytes " << sizeInBytes << endl;
 
 	//계산과정에서 버퍼 데이터가 필요한 경우가 있다면 메모리 할당
 	if (sizeInBytes != 0) checkCUDA(cudaMalloc(&workSpace, sizeInBytes));
