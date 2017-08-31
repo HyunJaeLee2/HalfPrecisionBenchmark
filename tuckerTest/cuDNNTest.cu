98ac930bbb6640a6d2f2bcc0435f81cd513e5eb6
Hyunjae Lee <dubu1993@snu.ac.kr>
2017-08-31 17:29:37 +0900

    tucker added

---
+++ tuckerTest/cuDNNTest.cu
@@ -0,0 +1,253 @@
+#include "util.hpp"
+#include "cuda.h"
+#include "cuDNNTest.h"
+
+using namespace std;
+
+const int batch_count = 1;
+long time_accum = 0;
+cudnnHandle_t cudnnHandle;
+bool isHalfPrecision;
+
+void free_layer(conv_layer *layer)
+{
+	checkCUDNN(cudnnDestroyTensorDescriptor(layer->inTensorDesc));
+	checkCUDNN(cudnnDestroyTensorDescriptor(layer->outTensorDesc));
+
+	checkCUDNN(cudnnDestroyConvolutionDescriptor(layer->convDesc));
+	checkCUDNN(cudnnDestroyFilterDescriptor(layer->filterDesc));
+
+    if(layer->d_inData) checkCUDA(cudaFree(layer->d_inData));
+    if(layer->d_outData) checkCUDA(cudaFree(layer->d_outData));;
+    if(layer->d_filterData) checkCUDA(cudaFree(layer->d_filterData));
+
+    if(layer->d_half_inData) checkCUDA(cudaFree(layer->d_half_inData));
+    if(layer->d_half_outData) checkCUDA(cudaFree(layer->d_half_outData));;
+    if(layer->d_half_filterData) checkCUDA(cudaFree(layer->d_half_filterData));
+    
+}
+
+conv_layer initFirstLayerWithRandom(int in_len, int in_channel, int filter_len, int filter_num)
+{
+	float *inData, *filterData;
+    int inSize, outSize, filterSize;
+    
+    conv_layer layer;
+
+    printf("in_len : %d, in_channel : %d, filter_len : %d, filter_num : %d\n", in_len, in_channel, filter_len, filter_num);
+    //set padding and stride to 1 as default
+    if(filter_len == 1) 
+        layer.padding_h = layer.padding_w = 0; 
+    else
+        layer.padding_h = layer.padding_w = 1;
+            
+    layer.stride_vertical = layer.stride_horizontal = 1;
+    
+    layer.in_height = layer.in_width = in_len; 
+    layer.in_channel = in_channel;
+    layer.filter_height = layer.filter_width = filter_len;
+    layer.filter_num = filter_num;
+    
+    //Init Size 
+    inSize = batch_count * layer.in_channel * layer.in_height * layer.in_width;
+    filterSize = layer.filter_num * layer.in_channel * layer.filter_height * layer.filter_width;
+    outSize = batch_count * layer.filter_num * layer.in_height * layer.in_width;
+    layer.outSize = outSize;
+    layer.inSize = inSize;
+    layer.filterSize = filterSize;
+
+    //Init Data
+    inData = (float *)malloc(sizeof(float) * inSize);
+    filterData = (float *)malloc(sizeof(float) * filterSize);
+
+    initWithRandom4D(inData, batch_count, layer.in_channel, layer.in_height, layer.in_width);
+    //print4D("In Data", inData, batch_count, layer.in_channel, layer.in_height, layer.in_width);
+
+    initWithRandom4D(filterData, layer.filter_num, layer.in_channel, layer.filter_height, layer.filter_width);
+    //print4D("Filter Data", filterData, layer.filter_num, layer.in_channel, layer.filter_height, layer.filter_width);
+    
+    checkCUDA(cudaMalloc((void**)&layer.d_inData, inSize * sizeof(float)));
+	checkCUDA(cudaMalloc((void**)&layer.d_filterData, filterSize * sizeof(float)));
+    checkCUDA(cudaMalloc((void**)&layer.d_outData, outSize * sizeof(float)));
+    
+	checkCUDA(cudaMemcpy(layer.d_inData, inData, inSize * sizeof(float), cudaMemcpyHostToDevice));
+	checkCUDA(cudaMemcpy(layer.d_filterData, filterData, filterSize * sizeof(float), cudaMemcpyHostToDevice));
+
+	checkCUDNN(cudnnCreateTensorDescriptor(&layer.inTensorDesc));
+	checkCUDNN(cudnnCreateTensorDescriptor(&layer.outTensorDesc));
+	checkCUDNN(cudnnCreateFilterDescriptor(&layer.filterDesc));
+	checkCUDNN(cudnnCreateConvolutionDescriptor(&layer.convDesc));
+    
+    const int convDims = 2;
+    int padA[convDims] = {layer.padding_h, layer.padding_w};
+    int filterStrideA[convDims] = {layer.stride_vertical, layer.stride_horizontal};
+    int upscaleA[convDims] = {1, 1};
+    int out_n, out_c, out_h, out_w;
+    
+    if(!isHalfPrecision)
+    {
+        checkCUDNN(cudnnSetTensor4dDescriptor(layer.inTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, batch_count, layer.in_channel, layer.in_height, layer.in_width));
+        checkCUDNN(cudnnSetFilter4dDescriptor(layer.filterDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, layer.filter_num, layer.in_channel, layer.filter_height, layer.filter_width));
+        
+        cudnnSetConvolutionNdDescriptor(layer.convDesc, convDims, padA, filterStrideA, upscaleA, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT); 
+        
+        checkCUDNN(cudnnGetConvolution2dForwardOutputDim(layer.convDesc, layer.inTensorDesc, layer.filterDesc, &out_n, &out_c, &out_h, &out_w));
+        
+        checkCUDNN(cudnnSetTensor4dDescriptor(layer.outTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, out_n, out_c, out_h, out_w));
+    }
+    else
+    {
+        checkCUDA(cudaMalloc((void**)&layer.d_half_inData, inSize * sizeof(__half)));
+        checkCUDA(cudaMalloc((void**)&layer.d_half_filterData, filterSize * sizeof(__half)));
+        checkCUDA(cudaMalloc((void**)&layer.d_half_outData, outSize * sizeof(__half)));
+
+        gpu_float2half_rn(inSize, layer.d_inData, layer.d_half_inData);
+        gpu_float2half_rn(filterSize, layer.d_filterData, layer.d_half_filterData);
+
+        checkCUDNN(cudnnSetTensor4dDescriptor(layer.inTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_HALF, batch_count, layer.in_channel, layer.in_height, layer.in_width));
+        checkCUDNN(cudnnSetFilter4dDescriptor(layer.filterDesc, CUDNN_DATA_HALF, CUDNN_TENSOR_NCHW, layer.filter_num, layer.in_channel, layer.filter_height, layer.filter_width));
+        
+        cudnnSetConvolutionNdDescriptor(layer.convDesc, convDims, padA, filterStrideA, upscaleA, CUDNN_CROSS_CORRELATION, CUDNN_DATA_HALF); 
+        
+        checkCUDNN(cudnnGetConvolution2dForwardOutputDim(layer.convDesc, layer.inTensorDesc, layer.filterDesc, &out_n, &out_c, &out_h, &out_w));
+        
+        checkCUDNN(cudnnSetTensor4dDescriptor(layer.outTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_HALF, out_n, out_c, out_h, out_w));
+        printf("HALF!!\n");
+    }
+    printf("conv out shape (n x c x h x w) = (%d x %d x %d x %d)\n", out_n, out_c, out_h, out_w);
+
+	checkCUDNN(cudnnGetConvolutionForwardAlgorithm(cudnnHandle,
+				layer.inTensorDesc,
+				layer.filterDesc,
+				layer.convDesc,
+				layer.outTensorDesc,
+				CUDNN_CONVOLUTION_FWD_PREFER_FASTEST,
+				0,
+				&layer.algo
+				));
+
+	cout << "Fastest layer.algorithm for conv0 = " << layer.algo << endl;
+
+	checkCUDNN(cudnnGetConvolutionForwardWorkspaceSize(cudnnHandle,
+				layer.inTensorDesc,
+				layer.filterDesc,
+				layer.convDesc,
+				layer.outTensorDesc,
+				layer.algo,
+				&layer.sizeInBytes));
+
+	cout << "sizeInBytes " << layer.sizeInBytes << endl;
+
+	if (layer.sizeInBytes != 0) checkCUDA(cudaMalloc(&layer.workSpace, layer.sizeInBytes));
+
+    return layer;
+}
+
+void executeLayer(int in_len, int in_channel, int filter_len, int filter_num)
+{
+    conv_layer conv1 = initFirstLayerWithRandom(in_len, in_channel, filter_len, filter_num);
+	
+    float *outData = (float *)malloc(sizeof(float) * conv1.outSize);
+
+	float alpha = 1.0f;
+	float beta = 0.0f;
+
+    long prev = timer_get();
+    long time_diff;
+    if(!isHalfPrecision)
+    {
+        checkCUDNN(cudnnConvolutionForward(cudnnHandle,
+                    &alpha,
+                    conv1.inTensorDesc,
+                    conv1.d_inData,
+                    conv1.filterDesc,
+                    conv1.d_filterData,
+                    conv1.convDesc,
+                    conv1.algo,
+                    conv1.workSpace,
+                    conv1.sizeInBytes,
+                    &beta,
+                    conv1.outTensorDesc,
+                    conv1.d_outData));
+        
+        checkCUDA(cudaDeviceSynchronize());
+
+        time_diff = (timer_get() - prev);
+        time_accum += time_diff; 
+
+        checkCUDA(cudaMemcpy(outData, conv1.d_outData, sizeof(float)* conv1.outSize, cudaMemcpyDeviceToHost));
+        //print4D("conv out", outData, 1, conv1.filter_num, conv1.in_height, conv1.in_width);
+
+        fprintf(stderr,   "[float  ]\t%9ld\n", time_diff);
+    }
+    else
+    {
+        checkCUDNN(cudnnConvolutionForward(cudnnHandle,
+                    &alpha,
+                    conv1.inTensorDesc,
+                    conv1.d_half_inData,
+                    conv1.filterDesc,
+                    conv1.d_half_filterData,
+                    conv1.convDesc,
+                    conv1.algo,
+                    conv1.workSpace,
+                    conv1.sizeInBytes,
+                    &beta,
+                    conv1.outTensorDesc,
+                    conv1.d_half_outData));
+
+        checkCUDA(cudaDeviceSynchronize());
+        
+        time_diff = (timer_get() - prev);
+        time_accum += time_diff; 
+
+        gpu_half2float(conv1.outSize, conv1.d_half_outData, conv1.d_outData);
+		checkCUDA(cudaMemcpy(outData, conv1.d_outData, sizeof(float)* conv1.outSize, cudaMemcpyDeviceToHost));
+		//print4D("conv out", outData, 1, conv1.filter_num, conv1.in_height, conv1.in_width);
+
+		fprintf(stderr,   "[half  ]\t%9ld\n", time_diff);
+    }
+
+}
+
+int main(int argc, char* argv[])
+{
+    int in_len, in_channel, filter_len, filter_num;
+    int rateS, rateT;
+    long time_save;
+
+    checkCUDNN(cudnnCreate(&cudnnHandle));
+    
+    if(argc < 5)
+    {
+        fprintf(stderr, "usage : ./test [Input Height/Width] [Channel] [Filter Height/Width] [Filter Num] [Rate of S] [Rate of T] [float/half]\n");
+        return 0;
+    }
+   
+    in_len =  atoi(argv[1]);
+    in_channel =  atoi(argv[2]);
+    filter_len =  atoi(argv[3]);
+    filter_num =  atoi(argv[4]);
+    rateS = atoi(argv[5]);
+    rateT = atoi(argv[6]);
+    
+    if(strcmp(argv[7], "half") == 0) 
+        isHalfPrecision = true;
+    else 
+        isHalfPrecision = false;
+    
+    executeLayer(in_len, in_channel, filter_len, filter_num);
+    
+    time_save = time_accum;
+    time_accum = 0;
+
+    executeLayer(in_len, in_channel, 1, in_channel * rateS / 100);
+    executeLayer(in_len, in_channel * rateS / 100, filter_len, filter_num * rateT / 100 );
+    executeLayer(in_len, filter_num * rateT / 100, 1, filter_num);
+
+    fprintf(stderr,   "[w/o tucker  ]\t%9ld\n", time_save);
+    fprintf(stderr,   "[apply tucker  ]\t%9ld\n", time_accum);
+    
+    checkCUDA(cudaThreadSynchronize());
+    return 0;
+}
---
+++ tuckerTest/util.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cuda.h>
+#include <cudnn.h>
+#include <stdio.h>
+#include "util.hpp"
+
+using namespace std;
+
+void checkCUDNN(cudnnStatus_t status)
+{
+	if (status != CUDNN_STATUS_SUCCESS)
+		cout << "[ERROR] CUDNN " << status << endl;
+}
+
+void checkCUDA(cudaError_t error)
+{
+	if (error != CUDA_SUCCESS)
+		cout << "[ERROR] CUDA " << error << endl;
+}
+
+void printCUDAMem()
+{
+    size_t free, total;
+    cudaMemGetInfo(&free,&total); 
+    printf("%ld KB free of total %ld KB\n",free/1024,total/1024);
+}
+
+long timer_get(void)
+{
+    struct timespec time;
+    if ( 0 == clock_gettime( CLOCK_REALTIME, &time)) {
+        //printf( "tv_sec: %ld\ntv_nsec: %ld", time.tv_sec, time.tv_nsec);
+    } else {
+        fprintf( stderr, "Something wrong on clock_gettime()\n");
+    }   
+    //return  time.tv_sec+time.tv_nsec*1e-9;
+    //return  (time.tv_nsec + time.tv_sec*1000000000)/1000;
+    return  (time.tv_nsec)/1000 + time.tv_sec*1000000;
+    //return (k1_io_read64(0x70084040)/MPPA_FREQUENCY);
+}
+
+void print4D(const char* title, float* src, int num, int channel, int height, int width)
+{
+    cout << title << endl;
+    for (int n = 0; n < min(2, num); n++) {
+        for (int i = 0; i < min(4, channel); i++) {
+            for (int y = 0; y < min(4, height); y++) {
+                for (int x = 0; x < min(4, width); x++) {
+                    printf("%.4f ", src[convertToIndex(n, num, i, channel, y, height, x, width)]);
+                }   
+                cout << endl;
+            }   
+            cout << endl;
+        }
+    } 
+}
+
+void initWithRandom4D(float* src, int num, int channel, int height, int width)
+{
+    for (int n = 0; n < num; n++) {
+        for (int i = 0; i < channel; i++) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    src[convertToIndex(n, num, i, channel, y, height, x, width)] = 
+                            static_cast <float> (rand()) / (static_cast <float> (RAND_MAX) * 10);
+                }   
+            }   
+        }
+    } 
+}
+int convertToIndex(int n, int batch_count, int k, int in_channel, int h, int in_height, int w, int in_width) 
+{
+    return ((n * in_channel + k) * in_height + h) * in_width + w;
+}
